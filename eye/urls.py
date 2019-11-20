3cad83aacf16c8c44dc7ba8f9003defad7e2b45a
soonju <poodle0403@likelion.org>
2019-11-20 20:05:40 +0900

    2nd

--- eye/urls.py
+++ eye/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+import mindapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',mindapp.views.home, name='home'),
+    path('result/',mindapp.views.result, name='result'),
 ]
--- mindapp/views.py
+++ mindapp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def result(request):
+    quiz1 = request.GET['quiz1']
+    dap = 0
+    if quiz1 == '슬픔':
+        dap += 1
+        return render(request,'result.html',{'dap':dap})
+    else:
+        return render(request,'result.html',{'dap':dap})
