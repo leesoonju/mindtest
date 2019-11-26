a890c49aab783933c076a3610b9597f61da9cc88
jangmii <0310_rose@naver.com>
2019-11-26 17:03:38 +0900

    진짜추가

--- static/js/test.js
+++ static/js/test.js
@@ -4,20 +4,23 @@ var 사진=['angry1','bored1','happy1','pain1','sad1','scary1','surprise1','pain
 var count=0;
 var 점수=0;
 var li =document.getElementsByTagName('li');
-for(var i=0;i<3;i+=1){
+var 퀴즈번호=document.getElementsByTagName('h1');
+
+퀴즈번호.innerText="Quiz"+str(count);
+for(var i=0;i<4;i+=1){
     li[i].innerText=보기[count][i];
 }
 function click(e,num){
 
-    if(보기[count][num]===정담[count]) 점수+=1;
-
+    if(보기[count][num]===정답[count]) 점수+=1;
 
     count+=1;
     if(count==10){
         doucument.getElementById('결과창').innerText=점수;
         return;
     }
-    for(var i=0;i<3;i+=1){
+    퀴즈번호.innerText="Quiz"+str(count);
+    for(var i=0;i<4;i+=1){
         li[i].innerText=보기[count][i];
     }
     
