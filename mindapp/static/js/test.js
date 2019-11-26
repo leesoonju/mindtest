7307b41a3b37a008d70c6bb7045139a52f829248
soonju <poodle0403@likelion.org>
2019-11-26 18:29:36 +0900

    55555

--- mindapp/static/js/test.js
+++ mindapp/static/js/test.js
@@ -1,31 +1,35 @@
 var 정답=['보기','보기','보기','보기','보기','보기','보기','보기','보기','보기'];
-var 보기=[['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기']];
+var 보기=[['보기','보기','보기','보기'],['보기1','보기2','보기3','보기4'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기']];
 var 사진=['angry1','bored1','happy1','pain1','sad1','scary1','surprise1','pain2','bored2','surprise2'];
 var count=0;
 var 점수=0;
 var li = document.getElementsByTagName('li');
-var 퀴즈번호=document.getElementsByTagName('h1');
+var 퀴즈번호=document.getElementsByTagName('h1')[0];
+var image=document.getElementsByTagName('img')[0];
 
-퀴즈번호.innerText="Quiz"+String(count+1);
-console.log(li)
+image.src= `static/${사진[count]}.jpg`;
+
+퀴즈번호.textContent="Quiz"+String(count+1);
 
 for(var i=0;i<4;i+=1){
-    li[i].innerText=보기[count][i];
-    console.log(li[i].innerText)
+    li[i].innerText=보기[count][i];    
 }
 
-function click(e,num){
 
+function click(e,num){
+    console.log('1')
     if(보기[count][num]===정답[count]) {
-        점수+=1;
+        점수+=1;        
     }
+    count+=1;
     if(count==10){
         doucument.getElementById('결과창').innerText=점수;
         return;
     }
-    퀴즈번호.innerText="Quiz"+str(count);
+    image.src= `static/${사진[count]}.jpg`;
+    퀴즈번호.innerText="Quiz"+String(count+1);
     for(var i=0;i<4;i+=1){
-        li[i].innerText=보기[count][i];
+        innerText=보기[count][i];
     }
 }
 
