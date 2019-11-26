43427f52793ff9a42e6805ba6a45dccfbb12ba0c
soonju <poodle0403@likelion.org>
2019-11-26 21:50:10 +0900

    완성

--- mindapp/static/js/test.js
+++ mindapp/static/js/test.js
@@ -1,34 +1,42 @@
-var 정답=['보기','보기','보기','보기','보기','보기','보기','보기','보기','보기'];
-var 보기=[['보기','보기','보기','보기'],['보기1','보기2','보기3','보기4'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기'],['보기','보기','보기','보기']];
-var 사진=['angry1','bored1','happy1','pain1','sad1','scary1','surprise1','pain2','bored2','surprise2'];
+var 정답=['화남','지루함','행복함','아픔','슬픔','무서움','놀람','아픔','지루함','놀람'];
+var 보기=[['화남','지루함','짜증','힘듦'],['졸림','지루함','즐거움','행복함'],
+        ['그리움','지루함','어이없음','행복함'],['연민','아픔','지겨움','놀람'],
+        ['슬픔','혐오','아픔','신남'],['무서움','화남','아픔','편안함'],
+        ['감격스러움','즐거움','공포','놀람'],['아픔','우울','불편함','즐거움'],
+        ['멍때림','짜증','졸림','지루함'],['신남','화남','놀람','혐오']];
+var 사진=['angry1','bored1','happy1','pain1','sad1','scary1','surprise','pain2','bored2','surprise2'];
+const 설명=['공감능력 매우부족 ㅠㅠ','공감능력 다소 부족ㅠ ','공감능력 우수!','공감능력 만점!']
 var count=0;
 var 점수=0;
 var li = document.getElementsByTagName('li');
 var 퀴즈번호=document.getElementsByTagName('h1')[0];
 var image=document.getElementsByTagName('img')[0];
-
-image.src= `static/${사진[count]}.jpg`;
-
-퀴즈번호.textContent="Quiz"+String(count+1);
-
-for(var i=0;i<4;i+=1){
-    li[i].innerText=보기[count][i];    
+reset(0);
+function reset(n){
+    if(n>9) return;
+    console.log(n);
+    image.src= `static/${사진[n]}.jpg`;
+    퀴즈번호.textContent="Quiz"+String(n+1);
+    for(var i=0;i<4;i+=1){
+        li[i].innerText=보기[n][i];    
+    }
 }
 
-
-function click(num){
-    console.log('1')
-    if(e.target.value===정답[count]) {
+function f(e){
+    if(보기[count][Number(e)]===정답[count]) {
         점수+=1;        
     }
     count+=1;
-    if(count==10){
-        doucument.getElementById('결과창').innerText=점수;
+    console.log(count,점수);
+    if(count===10){
+        document.getElementById('test').style.display="none";
+        document.getElementById('결과창').innerText=점수+'점 / 10점!!';
+        if(점수<=3) document.getElementById('설명').innerText=설명[0];
+        else if(점수<=5) document.getElementById('설명').innerText=설명[1];
+        else if(점수<=8) document.getElementById('설명').innerText=설명[2];
+        else document.getElementById('설명').innerText=설명[3];
         return;
     }
-    image.src= `static/${사진[count]}.jpg`;
-    퀴즈번호.innerText="Quiz"+String(count+1);
-    for(var i=0;i<4;i+=1){
-        innerText=보기[count][i];
-    }
+    reset(count);
+    
 }
